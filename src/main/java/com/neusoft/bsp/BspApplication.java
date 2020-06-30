110598fd5fdaabaa83d40a2c57baf38038a85f61
zenkaiii <wintersoul@foxmail.com>
2020-06-30 23:28:13 +0800

    init

---
+++ src/main/java/com/neusoft/bsp/BspApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.bsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BspApplication.class, args);
+    }
+
+}
